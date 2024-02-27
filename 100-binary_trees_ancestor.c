82b05b7c95c272df80b37e7fd9634027d7743339
nimiabaga <nimiabaga@yahoo.com>
2024-02-27 14:34:02 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,29 +7,31 @@
  *
  * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
-	binary_tree_t *binary_trees_ancestor
-(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	const binary_tree_t *ancestor = NULL;
+    const binary_tree_t *ancestor_second, *ancestor_first = first;
 
-	if (!first || !second)
-		return (NULL);
+    if (!first || !second)
+        return (NULL);
 
-	while (first)
-	{
-		const binary_tree_t *tmp = second;
+    /* Traverse upwards from the first/second node to the root */
+    while (ancestor_first)
+    {
+        ancestor_second = second;
 
-		while (tmp)
-		{
-			if (first == tmp)
-				return ((binary_tree_t *)first);
+        while (ancestor_second)
+        {
+            /* Check if a common ancestor is found */
+            if (ancestor_first == ancestor_second)
+                return ((binary_tree_t *)ancestor_first);
 
-			tmp = tmp->parent;
-		}
+            /* Move up to the parent of the second/first ancestor */
+            ancestor_second = ancestor_second->parent;
+        }
 
-		first = first->parent;
-	}
+        ancestor_first = ancestor_first->parent;
+    }
 
-	return ((binary_tree_t *)ancestor);
+    return (NULL);
 }
 
