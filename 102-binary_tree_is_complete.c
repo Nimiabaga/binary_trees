602fb4144b3fd03ebf1913b2d779b00a97ee3201
nimiabaga <nimiabaga@yahoo.com>
2024-02-27 15:50:03 +0100

    102-main.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -36,16 +36,23 @@ int is_complete(const binary_tree_t *tree, int index, int size)
 }
 
 /**
- * binary_tree_size - Measures the size of a binary tree.
- * @tree: Pointer to the root node of the tree to measure the size.
+ * binary_tree_size - Measures the size of a binary tree
  *
- * Return: Size of the tree, 0 if tree is NULL.
+ * @tree: Pointer to the root node of the tree to measure the size
+ *
+ * Return: Size of the tree, 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+        size_t size = 0;
 
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+        if (tree == NULL)
+                return (0);
+
+        size += binary_tree_size(tree->left);
+        size += binary_tree_size(tree->right);
+
+        return (size + 1);
 }
+
 
