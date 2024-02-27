452d450e950cb22712dae6eb01cb73b9fb392f8d
nimiabaga <nimiabaga@yahoo.com>
2024-02-27 16:29:54 +0100

    114-main.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,58 +1,125 @@
 #include "binary_trees.h"
 
-bst_t *bst_remove(bst_t *root, int value);
-
-
 /**
- * bst_remove - Removes a node from a Binary Search Tree (BST)
- * @root: Pointer to the root node of the tree where the node will be removed
- * @value: The value to remove from the tree
+ * find_node - Finds a node with a specific value in a Binary Search Tree (BST)
+ * @root: Pointer to the root node
+ * @value: Value to search for in the tree
  *
- * Return: A pointer to the new root node of the tree after removing the value
+ * Return: Pointer to node with the specified value, NULL otherwise
  */
-bst_t *bst_remove(bst_t *root, int value)
+bst_t *find_node(bst_t *root, int value)
 {
-
-	if (!root)
-
-		return (NULL);
+	if (!root || root->n == value)
+		return (root);
 
 	if (value < root->n)
+		return (find_node(root->left, value));
+	else
+		return (find_node(root->right, value));
+}
 
-		root->left = bst_remove(root->left, value);
-	else if (value > root->n)
-		root->right = bst_remove(root->right, value);
+/**
+ * find_successor - Finds the in-order successor of a node in a BST
+ * @node: Pointer to the node
+ *
+ * Return: Pointer to the in-order successor
+ */
+bst_t *find_successor(bst_t *node)
+{
+	bst_t *successor = node->right;
+
+	while (successor->left != NULL)
+		successor = successor->left;
+
+	return (successor);
+}
+
+/**
+ * remove_node - Removes a node from a Binary Search Tree (BST)
+ * @root: Pointer to the root node of the tree
+ * @node_to_remove: Pointer to the node to be removed
+ *
+ * Return: Pointer to the new root node
+ */
+bst_t *remove_node(bst_t *root, bst_t *node_to_remove)
+{
+	bst_t *new_node;
+
+	/* Different cases when removing a node */
+	if (!node_to_remove->left)
+	{
+		/* Node has no left child or one child */
+		if (node_to_remove->right != NULL)
+			node_to_remove->right->parent = node_to_remove->parent;
+
+		if (node_to_remove->parent == NULL)
+			root = node_to_remove->right;
+		else if (node_to_remove == node_to_remove->parent->left)
+			node_to_remove->parent->left = node_to_remove->right;
+		else
+			node_to_remove->parent->right = node_to_remove->right;
+
+		free(node_to_remove);
+	}
+	else if (!node_to_remove->right)
+	{
+		/* Node has no right child */
+		node_to_remove->left->parent = node_to_remove->parent;
+
+		if (!node_to_remove->parent)
+			root = node_to_remove->left;
+		else if (node_to_remove == node_to_remove->parent->left)
+			node_to_remove->parent->left = node_to_remove->left;
+		else
+			node_to_remove->parent->right = node_to_remove->left;
+
+		free(node_to_remove);
+	}
 	else
 	{
-		if (!root->left)
-		{
-			bst_t *temp = root->right;
+		/* Find the in-order successor */
+		new_node = find_successor(node_to_remove);
 
-			free(root);
+		/* Replace node with its in-order successor */
+		if (new_node->right != NULL)
+			new_node->right->parent = new_node->parent;
 
-			return (temp);
-		}
-		else if (!root->right)
+		if (!new_node->parent)
+			root = new_node->right;
+		else if (new_node == new_node->parent->left)
+			new_node->parent->left = new_node->right;
+		else
+			new_node->parent->right = new_node->right;
 
-		{
+		/* Copy value from the in-order successor to the original node */
+		node_to_remove->n = new_node->n;
 
-			bst_t *temp = root->left;
-
-			free(root);
-
-
-			return (temp);
-		}
-
-		bst_t *temp = root->right;
-
-		while (temp->left)
-			temp = temp->left;
-
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
+		free(new_node);
 	}
 
 	return (root);
+}
+
+/**
+ * bst_remove - Removes a node from a Binary Search Tree (BST)
+ * @root: Pointer to the root node of the tree
+ * @value: Value to remove in tree
+ *
+ * Return: Pointer to the new root node
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *node_to_remove;
+
+	if (!root)
+		return (NULL);
+
+	/* Find the node to be removed */
+	node_to_remove = find_node(root, value);
+
+	if (!node_to_remove)
+		return (root);
+
+	return (remove_node(root, node_to_remove));
 }
 
