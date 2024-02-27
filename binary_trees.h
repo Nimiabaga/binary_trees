d80517f6b0eb3832aa0138179d96707eec41126b
nimiabaga <nimiabaga@yahoo.com>
2024-02-27 15:58:44 +0100

    103-main.c

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void tree_level(const binary_tree_t *tree, size_t a, void (*func)(int));
 int is_complete(const binary_tree_t *tree, int index, int size);
 int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 
 
