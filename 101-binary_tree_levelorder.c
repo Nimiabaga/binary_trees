dbf5ccbcb4b0fa1f5cc304c9cc077f436b6c5cb2
nimiabaga <nimiabaga@yahoo.com>
2024-02-27 15:03:08 +0100

    101-main.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -3,39 +3,54 @@
 #include <stdio.h>
 
 /**
- * binary_tree_levelorder - Goes through a
- * binary tree using level-order traversal
+ * binary_tree_levelorder - Goes through a binary tree using level-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each
- * node (the value in the node is passed as a parameter to this function)
+ *       node (the value in the node is passed as a parameter to this function)
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
 
-	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) *
-			binary_tree_height(tree) * 2);
-	if (!queue)
-		return;
-
-	int front = 0, rear = 0;
-	binary_tree_t *current;
-
-	queue[rear++] = (binary_tree_t *)tree;
-
-	while (front < rear)
-	{
-		current = queue[front++];
-		func(current->n);
-
-		if (current->left)
-			queue[rear++] = current->left;
-
-		if (current->right)
-			queue[rear++] = current->right;
-	}
-
-	free(queue);
+	int height = binary_tree_height(tree);
+	for (int i = 1; i <= height; i++)
+		tree_level(tree, i, func);
 }
 
+/**
+ * tree_level - Recursive function to execute func on nodes at a specific level
+ * @tree: Pointer to the root of the tree
+ * @a: Tree level
+ * @func: Pointer to the function to execute
+ */
+void tree_level(const binary_tree_t *tree, int a, void (*func)(int))
+{
+	if (!tree)
+		return;
+	if (a == 1)
+		func(tree->n);
+	else if (a > 1)
+	{
+		tree_level(tree->left, a - 1, func);
+		tree_level(tree->right, a - 1, func);
+	}
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l, height_r;
+
+	if (tree == NULL)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (height_l > height_r ? height_l : height_r);
+}
