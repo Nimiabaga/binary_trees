999cdbd67b5959bb87fa00c789135129d52a3efc
nimiabaga <nimiabaga@yahoo.com>
2024-02-28 21:13:56 +0100

    121-avl_insert.c

--- binary_trees.h
+++ binary_trees.h
@@ -52,7 +52,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second);
+		const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void tree_level(const binary_tree_t *tree, size_t a, void (*func)(int));
 int is_complete(const binary_tree_t *tree, int index, int size);
@@ -71,7 +71,7 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 int validate_avl_tree(const binary_tree_t *tree, int lo, int hi);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *avl_insert_rec(avl_t **c_node, avl_t *p_node,
-                avl_t **n_node_ptr, int v_node);
+		avl_t **n_node_ptr, int v_node);
 
 
 
